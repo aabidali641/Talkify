5cc5bd816196836b3895ee97bc149c0b62d214e5
Aabid Ali <mdaabidali28@gmail.com>
2025-01-19 18:51:25 +0530

    fix2

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ import cors from "cors";
 import path from "path";
 import { app, server } from "./lib/socket.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); // Adjust the size as needed
@@ -23,7 +23,7 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => res.status(200).send("Server is running"));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
